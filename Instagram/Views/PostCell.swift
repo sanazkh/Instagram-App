9dc3760d0bdfd1d468f0df1e1931cc5f9d039c30
sanazkh <sanaz.khosravi70@gmail.com>
2018-10-02 09:18:42 -0700

    first commit

---
+++ Instagram/Views/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  Instagram
+//
+//  Created by Sanaz Khosravi on 10/1/18.
+//  Copyright © 2018 GirlsWhoCode. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
